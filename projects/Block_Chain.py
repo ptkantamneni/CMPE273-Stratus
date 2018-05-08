b302d10a12f6a0908b7a6fc9c40c08e5eed6f3bf
NikhileshChaudhary2903 <33855202+NikhileshChaudhary2903@users.noreply.github.com>
2018-05-08 07:54:44 -0700

    Update Block_Chain.py

--- projects/Block_Chain.py
+++ projects/Block_Chain.py
@@ -206,4 +206,19 @@ def mine():
     }
     return jsonify(response), 200
 
-    
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
